46d75f30e8dd010d6dc336c488a90588c0bcf193
Utkarsh Tiwari <79445012+UT-03@users.noreply.github.com>
2022-07-05 00:38:09 +0530

    feat: add Auth routes

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
 
+const AuthRoutes = require('./routes/Auth');
+const UserRoutes = require('./routes/User');
+const { addSockets } = require('./Socket/addSockets');
+
 // Initializing express app
 const app = express();
 
@@ -15,19 +19,33 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 // Routes here
+app.use('/api/auth', AuthRoutes);
+app.use('/api/user', UserRoutes);
 
+// Handling error
+app.use((error, req, res, next) => {
+    if (res.headerSent) {
+        return next(error);
+    }
+    res.status(error.code || 500);
+    res.json({ message: error.message || 'Something went wrong, please try again later.' });
+});
 
-let server;
 // Connecting to database
 mongoose.connect(process.env.MONGO_URL)
     .then(() => {
         // Starting server
-        server = app.listen(process.env.PORT);
+        return app.listen(process.env.PORT);
     })
-    .then(() => {
+    .then(server => {
+        // Adding socket.io
+        addSockets(server);
+
         // consoling result
         console.log('Connected to database and server started at port ', process.env.PORT);
     })
     .catch(err => {
         console.log(err);
     });
+
+
